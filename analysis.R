b793702b654e322e12d253729d4e5928e121bcc3
JackSchreuder <jackschreuder@icloud.com>
2022-05-18 14:47:10 -0700

    preliminary messing around

--- analysis.R
+++ analysis.R
@@ -3,3 +3,8 @@ library(dplyr)
 library(ggplot2)
 
 incarc_df <- read.csv("incarceration_trends.csv")
+state_df <- incarc_df %>% group_by(state, year) %>% summarise(total_incarcerated = sum(total_pop))
+highest_pop_states <- state_df %>% group_by(state) %>%
+  summarise(total_incarc = sum(total_incarcerated)) %>%
+  arrange(desc(total_incarc)) %>%
+  slice_head(n=5)
